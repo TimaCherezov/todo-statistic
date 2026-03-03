ac89525c265a9f3ef7883dd129e5af78460abdad
Dilliya <dilusha.37.writer@gmail.com>
2026-03-03 19:57:19 +0400

    Add sort

--- index.js
+++ index.js
@@ -38,7 +38,27 @@ function processCommand(command) {
                 }
             }
             break;
-
+        case 'sort':
+            switch (com[1]) {
+                case 'importance':
+                    const impComp = sortCommentsByImportance(comments);
+                    for (const comment of impComp) {
+                        console.log(comment)
+                    }
+                    break
+                case 'user':
+                    sortCommentsByUsers(comments);
+                    break
+                case 'date':
+                    const sBD = sortCommentsByDate(comments);
+                    for (const comment of sBD) {
+                        console.log(comment)
+                    }
+                    break
+                default:
+                    console.log('wrong command')
+            }
+            break;
         default:
             console.log('wrong command');
             break;
@@ -59,6 +79,80 @@ function getAllComments() {
         }
     }
     return comments;
+}
+
+function sortCommentsByImportance(comments) {
+    const pairs = []
+    for (const comment of comments) {
+        const count = [...comment].filter(s => s === '!').length;
+        pairs.push([count, comment]);
+    }
+    return pairs.sort((a, b) => b[0] - a[0]).map(p => p[1]);
+}
+
+function sortCommentsByUsers(comments) {
+    const userDict = new Map();
+    userDict.set('zzzzz', [])
+    for (const comment of comments) {
+        const parts = comment.split(';');
+        if (parts.length !== 3) {
+            userDict.get('zzzzz').push(comment);
+            continue;
+        }
+        const name = parts[0].split(' ')[2].toLowerCase();
+        if (!userDict.has(name)) {
+            userDict.set(name, []);
+        }
+        userDict.get(name).push(comment);
+    }
+    for (const user of userDict.keys()) {
+        if (user === 'zzzzz') {
+            continue;
+        }
+        for (const comment of userDict.get(user)) {
+            console.log(comment)
+        }
+    }
+    for (const comment of userDict.get('zzzzz')) {
+        console.log(comment)
+    }
+}
+
+function sortCommentsByDate(comments) {
+    const userDict = new Map();
+    userDict.set('NO_DATE', []);
+
+    for (const comment of comments) {
+        const parts = comment.split(';');
+
+        if (parts.length !== 3) {
+            userDict.get('NO_DATE').push(comment);
+            continue;
+        }
+
+        const dateStr = parts[1].trim();
+        const date = new Date(dateStr);
+
+        if (isNaN(date)) {
+            userDict.get('NO_DATE').push(comment);
+            continue;
+        }
+
+        if (!userDict.has(dateStr)) {
+            userDict.set(dateStr, []);
+        }
+
+        userDict.get(dateStr).push(comment);
+    }
+
+    return [
+        ...[...userDict.entries()]
+        .filter(([date]) => date !== 'NO_DATE')
+        .sort((a, b) => new Date(b[0]) - new Date(a[0]))
+        .map(([_, comments]) => comments)
+        .flat(),
+        ...userDict.get('NO_DATE')
+    ];
 }
 
 // TODO you can do it!
