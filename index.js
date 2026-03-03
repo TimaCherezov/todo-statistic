a6a54171c2b79271700acc0dd4e1d0fd44f72afb
Dilliya <dilusha.37.writer@gmail.com>
2026-03-03 18:52:15 +0400

    Add show

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
+const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
+const { readLine } = require('./console');
 
 const files = getFiles();
 
@@ -16,10 +16,33 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            comments = getAllComments();
+            debugger;
+            for (const comment of comments) {
+                console.log(comment);
+            }
+            break;
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function getAllComments() {
+    const comments = [];
+    const files = getFiles();
+    for (const fileText of files) {
+        const lines = fileText.split('\n');
+        for (const line of lines) {
+            const startComIndex = line.indexOf('// TODO ')
+            if (startComIndex === -1) {
+                continue;
+            }
+            comments.push(line.slice(startComIndex));
+        }
+    }
+    return comments;
 }
 
 // TODO you can do it!
